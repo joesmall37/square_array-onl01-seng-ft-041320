af2c1ec3a867904895ad3074ad69d2a473b25fa7
Joe Small <small.joseph@outlook.com>
2020-04-15 21:04:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
    new_array = []
-end 	  array.each do |num|
-    new_array << (num ** 2)
+end 	  array.each do |number|
+    new_array << number ** 2
   end
   
   return new_array
